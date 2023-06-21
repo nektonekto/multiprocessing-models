e74da8a18584e92f35da55246af3dffd332be76d
Vitaliy <bvv19r384@student.bmstu.ru>
2023-06-21 17:45:34 +0300

    Fix start

--- test.py
+++ test.py
@@ -5,6 +5,8 @@ import time
 import multiprocessing
 from multiprocessing import Process
 import os 
+from sklearn.metrics import mean_absolute_error
+
 # from sklearn.svm import SVC
 # from sklearn.datasets import load_diabetes, load_wine
 # from sklearn.metrics import RocCurveDisplay
@@ -18,10 +20,13 @@ with open('diab-8000.csv', 'r') as file1:
 print(multiprocessing.cpu_count())
 
 
-def fit(model, x, y, model_list):
+def fit(model, x, y, model_list, data):
     model.fit(x, y)
     model_list.append(model)
     print(f'Worker for model {model}')
+    print(model.predict(data))
+    # print(f'Модель: {mean_absolute_error(y_test, result)}')
+    
     return model
 
 
@@ -61,7 +66,7 @@ def model_svr_multiprocessing(df):
     # with multiprocessing.Pool(processes=2) as proc:
     cycle_time = time.time()
     for i in model_list:
-        proc = Process(target=fit, args=(i, X_train, y_train, trained_models))
+        proc = Process(target=fit, args=(i, X_train, y_train, trained_models, X_test))
         # proc = Process(target=i.fit(X_train, y_train), args=(i, X_train, y_train, trained_models))
         proc_list.append(proc)
         proc.start()
@@ -72,16 +77,15 @@ def model_svr_multiprocessing(df):
 
     
 
-    from sklearn.metrics import mean_absolute_error
-    for model in model_list:
-        result = model.predict(X_test)
-        print(f'Модель: {mean_absolute_error(y_test, result)}')
+    # for model in model_list:
+    #     result = model.predict(X_test)
+    #     print(f'Модель: {mean_absolute_error(y_test, result)}')
 
     # pred_model_1 = model.predict(X_test)
     # pred_model_2 = model_2.predict(X_test)
 
     end = time.time()
-    # print(f'Время выполнения: {end-start}')
+    print(f'Время выполнения: {end-start}')
     # print(f'Модель 1: {mean_absolute_error(y_test, pred_model_1)}\n'
         #   f'Модель 2: {mean_absolute_error(y_test, pred_model_2)}')
 
